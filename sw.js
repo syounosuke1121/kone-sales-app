25d63b8413ae197b2b9e884d6f1dfc62e3525b57
syounosuke1121 <134747482+syounosuke1121@users.noreply.github.com>
2026-07-15 19:38:07 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const C="kone-iphone-v1";
+const A=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
