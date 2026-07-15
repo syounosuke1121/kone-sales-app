85544a512002edb54d702b6ee7917f0f483765d2
syounosuke1121 <134747482+syounosuke1121@users.noreply.github.com>
2026-07-15 21:06:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,5 @@
-const C="kone-sales-v3";
+const C="kone-sales-v4";
 const A=["./","./index.html","./manifest.webmanifest","./icon-192.PNG","./icon-512.PNG"];
-self.addEventListener("install",e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(C).then(c=>c.addAll(A)));
-});
-self.addEventListener("activate",e=>{
-  e.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-self.addEventListener("fetch",e=>{
-  if(e.request.mode==="navigate"){
-    e.respondWith(fetch(e.request).catch(()=>caches.match("./index.html")));
-    return;
-  }
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{if(e.request.mode==="navigate"){e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>caches.match("./index.html")));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
